c457650cc305c0961b36acbc79d5fb3cc50424c0
Georg Limbach <glimbach@milabent.com>
2015-05-28 18:57:29 +0200

    Gesamtwertung: PDF und XLSX

--- app/helpers/score/competition_results_helper.rb
+++ app/helpers/score/competition_results_helper.rb
@@ -4,5 +4,24 @@ module Score
       @group_results ||= {}
       @group_results[gender] ||= Result.group_assessment_for(gender).decorate
     end
+
+    def table_data(gender, collection)
+      header = ["Punkte", "Mannschaft"]
+      group_results_for(gender).each do |result|
+        header.push(result.assessment.discipline.to_short)
+        header.push("")
+      end
+      rows = [header]
+      collection.each do |row|
+        current = [row.points.to_s, row.team.to_s]
+        group_results_for(gender).each do |result|
+          assessment_result = row.assessment_result_from(result.assessment)
+          current.push(assessment_result.try(:time).try(:decorate).to_s)
+          current.push(assessment_result.try(:points).to_s)
+        end
+        rows.push(current)
+      end
+      rows
+    end
   end
 end
