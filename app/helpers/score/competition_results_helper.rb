95346a2a8742aaeec51bf2cb05cce884c8919b90
Georg Limbach <glimbach@milabent.com>
2015-06-09 08:54:44 +0200

    Gesamtwertung: PDF und XLSX reparieren (closes #12)

--- app/helpers/score/competition_results_helper.rb
+++ app/helpers/score/competition_results_helper.rb
@@ -1,20 +1,16 @@
 module Score
   module CompetitionResultsHelper
-    def group_results_for gender
-      @group_results ||= {}
-      @group_results[gender] ||= Result.group_assessment_for(gender).decorate
-    end
-
-    def table_data(gender, collection)
+    def table_data(competition_result)
       header = ["Punkte", "Mannschaft"]
-      group_results_for(gender).each do |result|
+      competition_result.results.each do |result|
         header.push(result.assessment.discipline.to_short)
         header.push("")
       end
       rows = [header]
-      collection.each do |row|
+
+      competition_result.rows.each do |row|
         current = [row.points.to_s, row.team.to_s]
-        group_results_for(gender).each do |result|
+        competition_result.results.each do |result|
           assessment_result = row.assessment_result_from(result.assessment)
           current.push(assessment_result.try(:time).try(:decorate).to_s)
           current.push(assessment_result.try(:points).to_s)
---
+++ app/views/score/competition_results/index.pdf.prawn
@@ -0,0 +1,16 @@
+first = true
+@score_competition_results.each do |result|
+  pdf.start_new_page unless first
+  first = false
+  pdf_header pdf, "Gesamtwertung - #{result}"
+
+  pdf.table(table_data(result), {
+    header: true,
+    width: pdf.bounds.width,
+    cell_style: { align: :center},
+  }) do
+    row(0).style(font_style: :bold)
+  end
+end
+
+pdf_footer pdf
