c7f116d465586ca0b56397337c2d559500e22977
Georg Limbach <glimbach@milabent.com>
2015-06-16 22:30:21 +0200

    Debug-Ausgabe entfernen

--- spec/support/capybara_helper.rb
+++ spec/support/capybara_helper.rb
@@ -1,6 +1,5 @@
 def perform_login password="my-password"
   visit root_path
-  p User.first
   click_on "Login"
   expect(page).to have_content "Anmeldung im System"
   fill_in "Passwort", with: password
