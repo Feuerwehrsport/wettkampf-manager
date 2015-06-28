6bddd4b374226914265411150d764f354a02adb4
Georg Limbach <glimbach@milabent.com>
2015-06-28 22:50:56 +0200

    Seed von Wettkampf erzeugbar machen

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,29 @@ competition.save!
 
 user = User.new()
 user.save!(validate: false)
+
+
+# Team.transaction do
+#   old_logger = ActiveRecord::Base.logger
+#   ActiveRecord::Base.logger = nil
+
+#   puts "Assessment.transaction do"
+#   puts "hb_female = Assessment.discipline(Disciplines::ObstacleCourse.new).gender(:female).first"
+#   puts "hb_male = Assessment.discipline(Disciplines::ObstacleCourse.new).gender(:male).first"
+#   puts "hl_female = Assessment.discipline(Disciplines::ClimbingHookLadder.new).gender(:female).first"
+#   puts "hl_male = Assessment.discipline(Disciplines::ClimbingHookLadder.new).gender(:male).first"
+#   Team.includes(:people).all.each do |team|
+#     puts "team = Team.create!(name: '#{team.name}', gender: '#{team.gender}', number: #{team.number})"
+#     team.people.each do |person|
+#       puts "person = Person.create!(last_name: '#{person.last_name}', first_name: '#{person.first_name}', gender: '#{person.gender}', team: team, youth: #{person.youth}, fire_sport_statistics_person_id: #{person.fire_sport_statistics_person_id.inspect})"
+#       person.requests.each do |request|
+#         assessment = request.assessment.discipline.is_a?(Disciplines::ObstacleCourse) ? 'hb' : 'hl'
+#         assessment += "_#{person.gender}"
+#         puts "AssessmentRequest.create!(assessment: #{assessment}, entity: person, assessment_type: '#{request.assessment_type}', group_competitor_order: #{request.group_competitor_order}, relay_count: #{request.relay_count}, single_competitor_order: #{request.single_competitor_order})"
+#       end
+#     end
+#   end
+#   puts "end"
+
+#   ActiveRecord::Base.logger = old_logger
+# end
