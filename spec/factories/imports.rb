86a1c23e9255a450828adb3cebaeaa7a371f8e55
Georg Limbach <glimbach@milabent.com>
2016-03-07 20:15:33 +0100

    Tests für Import

---
+++ spec/factories/imports.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :imports_configuration, class: Imports::Configuration do
+    file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'import.wettkampf_manager_import')) }
+  end
+end
