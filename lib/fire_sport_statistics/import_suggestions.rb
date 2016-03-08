9b9c76165e7acdabe23e231f10143cfb8d0a180f
Georg Limbach <glimbach@milabent.com>
2016-03-08 21:55:59 +0100

    Prozessanzeige beim Import verbessern

--- lib/fire_sport_statistics/import_suggestions.rb
+++ lib/fire_sport_statistics/import_suggestions.rb
@@ -1,8 +1,6 @@
 module FireSportStatistics
   class ImportSuggestions
-    def initialize(verbose = false)
-      @verbose = verbose
-
+    def initialize
       ActiveRecord::Base.transaction do
         TeamAssociation.destroy_all
         TeamSpelling.destroy_all
@@ -12,57 +10,58 @@ module FireSportStatistics
 
         people = []
         teams = []
-        API::Get.fetch(:people).each do |person|
+        fetch(:people) do |person|
           people[person.id.to_i] = Person.create!(
             last_name: person.last_name,
             first_name: person.first_name,
             gender: person.gender,
             external_id: person.id,
           )
-          verbose_dot "p"
         end
 
-        API::Get.fetch(:teams).each do |team|
+        fetch(:teams) do |team|
           teams[team.id.to_i] = Team.create!(
             name: team.name,
             short: team.shortcut,
             external_id: team.id,
           )
-          verbose_dot "t"
         end
         
-        API::Get.fetch(:team_members).each do |association|
+        fetch(:team_members) do |association|
           TeamAssociation.create!(
             person: people[association.person_id.to_i],
             team: teams[association.team_id.to_i],
           )
-          verbose_dot "a"
         end
         
-        API::Get.fetch(:team_spellings).each do |spelling|
+        fetch(:team_spellings) do |spelling|
           TeamSpelling.create!(
             team: teams[spelling.team_id.to_i],
             name: spelling.name,
             short: spelling.shortcut,
           )
-          verbose_dot "s"
         end
         
-        API::Get.fetch(:person_spellings).each do |spelling|
+        fetch(:person_spellings) do |spelling|
           PersonSpelling.create!(
             person: people[spelling.person_id.to_i],
             last_name: spelling.last_name,
             first_name: spelling.first_name,
             gender: spelling.gender,
           )
-          verbose_dot "v"
         end
-        puts "" if @verbose
       end
     end
 
-    def verbose_dot char
-      print char if @verbose
+    def fetch(key)
+      collection = API::Get.fetch(key)
+      all_count = collection.count.to_f
+      collection.each_with_index do |entry, index|
+        yield(entry)
+        print "#{key}: #{(index.to_f/all_count*100).round}%\r"
+        STDOUT.flush
+      end
+      puts ""
     end
   end
 end
--- lib/tasks/import_suggestions.rake
+++ lib/tasks/import_suggestions.rake
@@ -1,11 +1,11 @@
 namespace :import do
   desc "Import fire sport statistics suggestions"
   task :suggestions, [] => :environment do |task, args|
-    FireSportStatistics::ImportSuggestions.new true
+    FireSportStatistics::ImportSuggestions.new
   end
 
   desc "Import fire sport statistics suggestions"
   task :d_cup_results, [] => :environment do |task, args|
-    FireSportStatistics::ImportDCupResults.new true
+    FireSportStatistics::ImportDCupResults.new
   end
 end
