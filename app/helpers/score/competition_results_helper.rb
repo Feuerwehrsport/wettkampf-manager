df3386ab290742683ec9a735a5e14f33d711fa20
Georg Limbach <glimbach@milabent.com>
2016-07-05 18:02:32 +0200

    PDF der Tageswertung korrigieren

--- app/helpers/score/competition_results_helper.rb
+++ app/helpers/score/competition_results_helper.rb
@@ -12,7 +12,7 @@ module Score::CompetitionResultsHelper
       current = [row.points.to_s, team]
       competition_result.results.each do |result|
         assessment_result = row.assessment_result_from(result.assessment)
-        current.push(assessment_result.try(:time).try(:decorate).to_s)
+        current.push(assessment_result.try(:result_entry).try(:decorate).to_s)
         current.push(assessment_result.try(:points).to_s)
       end
       rows.push(current)
