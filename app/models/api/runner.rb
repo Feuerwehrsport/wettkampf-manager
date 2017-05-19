6929251be4e1092298cf34f3c7342ce8dbe8d458
Georg Limbach <georg.limbach@lichtbit.com>
2017-05-19 10:33:28 +0200

    API-Eingabe erleichtern

--- app/models/api/runner.rb
+++ app/models/api/runner.rb
@@ -3,37 +3,50 @@ require 'rbconfig'
 class API::Runner
   
   def initialize
+    config = get_config
     cli.say("\n\n")
 
     klass = nil
+
+    default = config[:klass_select].present? ? " |#{config[:klass_select]}|" : ''
     cli.choose do |menu|
-      menu.prompt = 'Bitte das angeschlossene Gerät angeben:  '
-      menu.choice('Timy von Alge-Timing') { klass = API::TimyReader }
-      menu.choice('Computer vom Team-MV') { klass = API::TeamComputerReader }
+      menu.prompt = "Bitte das angeschlossene Gerät angeben#{default}: "
+      menu.choice('Timy von Alge-Timing') { |a| config[:klass_select] = a; klass = API::TimyReader }
+      menu.choice('Computer vom Team-MV') { |a| config[:klass_select] = a; klass = API::TeamComputerReader }
+      menu.default = config[:klass_select]
     end
     cli.say("\n")
 
-    serial_connection = cli.ask('Angeschlossene Schnittstelle? ') { |q| q.default = (os == :posix) ? '/dev/ttyUSB0' : 'COM0' }
+    default = config[:serial_connection] || ((os == :posix) ? '/dev/ttyUSB0' : 'COM0')
+    serial_connection = cli.ask('Angeschlossene Schnittstelle? ') { |q| q.default = default }
+    config[:serial_connection] = serial_connection
     cli.say("\n")
 
     sender = ''
+    default = config[:sender_select].present? ? " |#{config[:sender_select]}|" : ''
     cli.choose do |menu|
-      menu.prompt = 'Zeiten der Disziplin:  '
+      menu.prompt = "Zeiten der Disziplin#{default}:  "
       Discipline.types_with_key.each do |key, klass|
         next if key == :zk
-        menu.choice(klass.model_name.human) { sender = klass.model_name.human }
+        menu.choice(klass.model_name.human) { |a| config[:sender_select] = a; sender = klass.model_name.human }
       end
-      menu.choice('Anderes') do
+      menu.choice('Anderes') do |a| 
+        config[:sender_select] = a;
         sender = cli.ask('Name der Disziplin? ') { |q| q.default = klass::DEFAULT_SENDER }
       end
+      menu.default = config[:sender_select]
     end
     cli.say("\n")
 
-    url = cli.ask('URL zu Wettkampf-Manager? ') { |q| q.default = (os == :posix) ? 'http://localhost:3000' : 'http://localhost' }
+    default = config[:url] || ((os == :posix) ? 'http://localhost:3000' : 'http://localhost')
+    url = cli.ask('URL zu Wettkampf-Manager? ') { |q| q.default = default }
+    config[:url] = url
     cli.say("\n")
 
     password = cli.ask('Admin-Passwort für Wettkampf-Manager:  ') { |q| q.echo = '*' }
     cli.say("\n")
+
+    write_config(config)
 
     klass.start_with_check(url: url, password: password, serial_connection: serial_connection, cli: cli, sender: sender)
   end
@@ -52,5 +65,27 @@ class API::Runner
         :posix
       end
     )
+  end
+
+  protected
+
+  def config_file_path
+    "#{Rails.root}/api_runner.config"
+  end
+
+  def write_config(hash)
+    File.write(config_file_path, hash.to_json)
+  end
+
+  def get_config
+    if File.file?(config_file_path)
+      begin
+        JSON.parse(File.read(config_file_path), symbolize_names: true)
+      rescue JSON::ParserError => e
+        {}
+      end
+    else
+    {}
+    end
   end
 end
