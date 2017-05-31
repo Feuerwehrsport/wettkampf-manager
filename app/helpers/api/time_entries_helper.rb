a0aceb167b0a7c459a4122229a4c86ea9857d830
Georg Limbach <georg.limbach@lichtbit.com>
2017-05-31 08:55:41 +0200

    API-Zeiten für Mannschaftsdiziplinen korrigieren

--- app/helpers/api/time_entries_helper.rb
+++ app/helpers/api/time_entries_helper.rb
@@ -3,9 +3,9 @@ module API::TimeEntriesHelper
     @list_entry.list.assessments.first.discipline.single_discipline?
   end
 
-  # def multiple_assessments?
-  #   @score_list.assessments.count > 1
-  # end
+  def multiple_assessments?
+    @list_entry.list.assessments.count > 1
+  end
 
   def list_entry_options track, entry
     {}
