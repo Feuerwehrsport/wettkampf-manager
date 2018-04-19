39ee71a5588ae3616b11331a04cf6cebdb13b011
Georg Limbach <georg.limbach@lichtbit.com>
2018-04-19 08:57:17 +0200

    Sortierung korrigieren

--- app/decorators/score/competition_result_row_decorator.rb
+++ app/decorators/score/competition_result_row_decorator.rb
@@ -1,4 +1,8 @@
 class Score::CompetitionResultRowDecorator < ApplicationDecorator
   decorates_association :team
   decorates_association :assessment_results
+
+  def <=>(other)
+    object <=> other.object
+  end
 end
--- app/decorators/score/group_result_row_decorator.rb
+++ app/decorators/score/group_result_row_decorator.rb
@@ -3,4 +3,8 @@ class Score::GroupResultRowDecorator < ApplicationDecorator
   decorates_association :result_entry
   decorates_association :rows_in
   decorates_association :rows_out
+
+  def <=>(other)
+    object <=> other.object
+  end
 end
