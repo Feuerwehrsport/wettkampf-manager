d930baf08dfa682ae3275402ec4d8338b9d2c96a
Georg Limbach <georg.limbach@lichtbit.com>
2018-05-31 23:31:17 +0200

    Parameter an Linux-Skript übergebbar machen
    \curl -sSL https://feuerwehrsport-statistik.de/wk-linux-install |
    bash -s SKIP_INSTALL_APT path-to-install

--- doc/scripts/basic-install.sh
+++ doc/scripts/basic-install.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 set -e
+INSTALL_APT=yes
+INSTALL_PATH=wettkampf-manager
+INSTALL_PATH_ARG="$1"
 
-if [ $(which sudo | wc -l) -lt 1 ] ; then
-  echo 'sudo is not installed'
-  exit 1
+if [[ "$1" == "SKIP_INSTALL_APT" ]] ; then
+  INSTALL_APT=no
+  INSTALL_PATH_ARG="$2"
+  echo 'Installieren von Paketen wird übersprungen'
 fi
 
-sudo apt-get -y install nodejs curl git dirmngr bzip2 gawk g++ gcc make libc6-dev zlib1g-dev libyaml-dev \
-  libsqlite3-dev sqlite3 autoconf libgmp-dev libgdbm-dev libncurses5-dev automake libtool bison pkg-config \
-  libffi-dev libgmp-dev libreadline-dev libssl-dev
+if [[ "$INSTALL_PATH_ARG" != "" ]] ; then
+  INSTALL_PATH="$INSTALL_PATH_ARG"
+  echo "Installiere zu Pfad: $INSTALL_PATH"
+fi
+
+if [[ "$INSTALL_APT" == "yes" ]] ; then
+  if [ $(which sudo | wc -l) -lt 1 ] ; then
+    echo 'sudo is not installed'
+    exit 1
+  fi
+
+  sudo apt-get -y install nodejs curl git dirmngr bzip2 gawk g++ gcc make libc6-dev zlib1g-dev libyaml-dev \
+    libsqlite3-dev sqlite3 autoconf libgmp-dev libgdbm-dev libncurses5-dev automake libtool bison pkg-config \
+    libffi-dev libgmp-dev libreadline-dev libssl-dev
+fi
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --autolibs=read-fail
@@ -16,9 +32,9 @@ echo 'source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
 source "$HOME/.rvm/scripts/rvm"
 
 rvm install ruby-2.4.4
-git clone -b release --recursive https://github.com/Feuerwehrsport/wettkampf-manager.git
+git clone -b release --recursive https://github.com/Feuerwehrsport/wettkampf-manager.git "$INSTALL_PATH"
 
-cd wettkampf-manager/
+cd "$INSTALL_PATH"
 doc/scripts/install.sh
 
 echo ''
