f3146236ccc2bc6242aeb286f76c687f07af5abc
Georg Limbach <georg.limbach@lichtbit.com>
2019-08-28 15:17:49 +0200

    Anzahl der Starter bei Startlistenerstellung korrigieren

--- app/decorators/assessment_decorator.rb
+++ app/decorators/assessment_decorator.rb
@@ -6,19 +6,27 @@ class AssessmentDecorator < ApplicationDecorator
   end
 
   def name_with_request_count
-    if object.discipline.like_fire_relay? && assessment.requests.present?
+    if object.discipline.like_fire_relay? && related_requests.present?
       numbers = []
-      (1..assessment.requests.map(&:relay_count).max).each do |number|
-        count = assessment.requests.where('relay_count >= ?', number).count
+      (1..related_requests.map(&:relay_count).max).each do |number|
+        count = related_requests.where('relay_count >= ?', number).count
         numbers.push("#{count}x #{(64 + number).chr}")
       end
       "#{self} (#{numbers.join(', ')})"
     else
-      "#{self} (#{requests.count} Starter)"
+      "#{self} (#{related_requests.count} Starter)"
     end
   end
 
   def translated_gender
     gender.present? ? t("gender.#{gender}") : ''
+  end
+
+  def related_requests
+    if object.discipline.group_discipline?
+      object.requests.where(entity_type: 'Team')
+    else
+      object.requests.where(entity_type: 'Person')
+    end
   end
 end
