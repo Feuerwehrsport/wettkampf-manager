b650ef0955d24d2beadc0df34567dca3b2a437ac
Georg Limbach <georg.limbach@lichtbit.com>
2020-08-24 16:50:47 +0200

    CountTable aufräumen

--- app/lib/ui/count_table.rb
+++ app/lib/ui/count_table.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
-UI::CountTable = Struct.new(:view, :rows, :options, :columns, :data_fields) do
+UI::CountTable = Struct.new(:view, :rows, :options, :columns) do
   def initialize(*args)
     super
     self.options ||= {}
     self.columns = []
-    self.data_fields = []
-    before_initialize if respond_to? :before_initialize
     yield self
-    after_initialize if respond_to? :after_initialize
   end
 
   def col(column_head, column_key = nil, options = {}, &block)
@@ -16,46 +13,14 @@ UI::CountTable = Struct.new(:view, :rows, :options, :columns, :data_fields) do
       options = column_key
       column_key = nil
     end
-    column_key = [column_key] unless column_key.is_a? Array
-    column_key.compact!
     columns.push UI::Column.new(self, column_head, column_key, options, block)
   end
-
-  def data(data_name, &block)
-    data_fields.push(DataField.new(data_name, block))
-  end
-
-  def row_data(row)
-    data = {}
-    data_fields.each do |field|
-      data[field.name] = field.block.call(row)
-    end
-    data
-  end
-
-  def headlines
-    columns.map(&:name)
-  end
 end
-UI::DataField = Struct.new(:name, :block)
-UI::Column = Struct.new(:count_table, :name, :keys, :options, :block) do
-  def content(row)
-    if keys.present?
-      value = row
-      keys.each { |key| value = value.try(key) }
 
-      if options[:link_to].present?
-        to = options[:link_to] == true ? row : row.try(options[:link_to])
-        count_table.view.link_to(value, to)
-      else
-        value
-      end
-    elsif options[:helper_method].present?
-      if options[:helper_method_options].present?
-        count_table.view.send(options[:helper_method], row, options[:helper_method_options])
-      else
-        count_table.view.send(options[:helper_method], row)
-      end
+UI::Column = Struct.new(:count_table, :name, :key, :options, :block) do
+  def content(row)
+    if key.present?
+      row.try(key)
     else
       block.call(row)
     end
@@ -63,9 +28,5 @@ UI::Column = Struct.new(:count_table, :name, :keys, :options, :block) do
 
   def th_options
     options[:th_options] || {}
-  end
-
-  def th_value
-    th_options[:link_to] ? count_table.view.link_to(name, th_options[:link_to]) : name
   end
 end
