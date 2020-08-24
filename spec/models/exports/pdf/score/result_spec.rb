fc7cf4f13c5f73d4c02f86be444cdbf11ff1a3ec
Georg Limbach <georg.limbach@lichtbit.com>
2020-08-24 16:23:07 +0200

    Test ergänzen

--- spec/models/exports/pdf/score/result_spec.rb
+++ spec/models/exports/pdf/score/result_spec.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Exports::PDF::Score::Result, type: :model do
   let(:show_pdf) { described_class.perform(result.decorate, only) }
-  let(:person1) { create(:person, :generated) }
-  let(:person2) { create(:person, :generated) }
+  let(:person1) { create(:person, :generated, team: team) }
+  let(:person2) { create(:person, :generated, team: team) }
+  let(:team) { nil }
   let(:assessment) { create(:assessment) }
   let(:result) { create(:score_result, assessment: assessment, group_assessment: true) }
   let!(:list) { create_score_list(result, person1 => 2200, person2 => nil) }
@@ -18,6 +19,22 @@ RSpec.describe Exports::PDF::Score::Result, type: :model do
       expect(show_pdf.bytestream.size).to be_within(52_575).of(2000)
 
       expect(show_pdf.filename).to eq 'hakenleitersteigen-manner.pdf'
+    end
+
+    context 'when group result present' do
+      let(:team) { create(:team) }
+
+      before do
+        Competition.one.update!(group_assessment: true, group_score_count: 1)
+      end
+
+      it 'creates pdf' do
+        expect(show_pdf.bytestream).to start_with '%PDF-1.4'
+        expect(show_pdf.bytestream).to end_with "%%EOF\n"
+        expect(show_pdf.bytestream.size).to be_within(69_795).of(2000)
+
+        expect(show_pdf.filename).to eq 'hakenleitersteigen-manner.pdf'
+      end
     end
 
     context 'when only is group_assessment' do
