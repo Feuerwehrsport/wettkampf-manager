744b9e143619f7aa6baecf7e007fba129b432c81
Georg Limbach <georg.limbach@lichtbit.com>
2020-08-26 14:17:23 +0200

    Tests ergänzen

--- spec/models/exports/xlsx/score/result_spec.rb
+++ spec/models/exports/xlsx/score/result_spec.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Exports::XLSX::Score::Result, type: :model do
   let(:show_xlsx) { described_class.perform(result.decorate) }
-  let(:person1) { create(:person, :generated) }
-  let(:person2) { create(:person, :generated) }
+  let(:person1) { create(:person, :generated, team: team) }
+  let(:person2) { create(:person, :generated, team: team) }
+  let(:team) { nil }
   let(:assessment) { create(:assessment) }
   let(:result) { create(:score_result, assessment: assessment, group_assessment: true) }
   let!(:list) { create_score_list(result, person1 => 2200, person2 => nil) }
@@ -17,6 +18,22 @@ RSpec.describe Exports::XLSX::Score::Result, type: :model do
       expect(show_xlsx.bytestream.size).to be_within(100).of(4623)
 
       expect(show_xlsx.filename).to eq 'hakenleitersteigen-manner.xlsx'
+    end
+
+    context 'when group result present' do
+      let(:team) { create(:team) }
+
+      before do
+        Competition.one.update!(group_assessment: true, group_score_count: 1)
+      end
+
+      it 'creates pdf' do
+        expect(show_xlsx.bytestream).to start_with "PK\u0003"
+        expect(show_xlsx.bytestream).to end_with "\u0000\u0000"
+        expect(show_xlsx.bytestream.size).to be_within(100).of(4832)
+
+        expect(show_xlsx.filename).to eq 'hakenleitersteigen-manner.xlsx'
+      end
     end
   end
 end
